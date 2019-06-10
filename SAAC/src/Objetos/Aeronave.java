bc715fb15e929c8f7174b4854fdac47b83a6dce5
LeonardoFrancosi <47803975+LeonardoFrancosi@users.noreply.github.com>
2019-06-09 23:48:14 -0300

    re-enviando

---
+++ SAAC/src/Objetos/Aeronave.java
@@ -0,0 +1,114 @@
+
+public class Aeronave {
+	public int IdAeronave;
+	public String ModAeronave;
+	public double EnvAeronave;
+	public double ConAeronave;
+	public double BasAeronave;
+	public double BitAeronave;
+	public int MotoAeronave;
+	public double PaxAeronave;
+	public double PmdAeronave;
+	public double PmaAeronave;
+	public double PovAeronave;
+	public double PzcAeronave;
+	public double CbpAeronave;
+	public Aeronave(int idAeronave, String modAeronave, double envAeronave, double conAeronave, double basAeronave,
+			double bitAeronave, int motoAeronave, double paxAeronave, double pmdAeronave, double pmaAeronave,
+			double povAeronave, double pzcAeronave, double cbpAeronave) {
+		super();
+		IdAeronave = idAeronave;
+		ModAeronave = modAeronave;
+		EnvAeronave = envAeronave;
+		ConAeronave = conAeronave;
+		BasAeronave = basAeronave;
+		BitAeronave = bitAeronave;
+		MotoAeronave = motoAeronave;
+		PaxAeronave = paxAeronave;
+		PmdAeronave = pmdAeronave;
+		PmaAeronave = pmaAeronave;
+		PovAeronave = povAeronave;
+		PzcAeronave = pzcAeronave;
+		CbpAeronave = cbpAeronave;
+	}
+	public int getIdAeronave() {
+		return IdAeronave;
+	}
+	public void setIdAeronave(int idAeronave) {
+		IdAeronave = idAeronave;
+	}
+	public String getModAeronave() {
+		return ModAeronave;
+	}
+	public void setModAeronave(String modAeronave) {
+		ModAeronave = modAeronave;
+	}
+	public double getEnvAeronave() {
+		return EnvAeronave;
+	}
+	public void setEnvAeronave(double envAeronave) {
+		EnvAeronave = envAeronave;
+	}
+	public double getConAeronave() {
+		return ConAeronave;
+	}
+	public void setConAeronave(double conAeronave) {
+		ConAeronave = conAeronave;
+	}
+	public double getBasAeronave() {
+		return BasAeronave;
+	}
+	public void setBasAeronave(double basAeronave) {
+		BasAeronave = basAeronave;
+	}
+	public double getBitAeronave() {
+		return BitAeronave;
+	}
+	public void setBitAeronave(double bitAeronave) {
+		BitAeronave = bitAeronave;
+	}
+	public int getMotoAeronave() {
+		return MotoAeronave;
+	}
+	public void setMotoAeronave(int motoAeronave) {
+		MotoAeronave = motoAeronave;
+	}
+	public double getPaxAeronave() {
+		return PaxAeronave;
+	}
+	public void setPaxAeronave(double paxAeronave) {
+		PaxAeronave = paxAeronave;
+	}
+	public double getPmdAeronave() {
+		return PmdAeronave;
+	}
+	public void setPmdAeronave(double pmdAeronave) {
+		PmdAeronave = pmdAeronave;
+	}
+	public double getPmaAeronave() {
+		return PmaAeronave;
+	}
+	public void setPmaAeronave(double pmaAeronave) {
+		PmaAeronave = pmaAeronave;
+	}
+	public double getPovAeronave() {
+		return PovAeronave;
+	}
+	public void setPovAeronave(double povAeronave) {
+		PovAeronave = povAeronave;
+	}
+	public double getPzcAeronave() {
+		return PzcAeronave;
+	}
+	public void setPzcAeronave(double pzcAeronave) {
+		PzcAeronave = pzcAeronave;
+	}
+	public double getCbpAeronave() {
+		return CbpAeronave;
+	}
+	public void setCbpAeronave(double cbpAeronave) {
+		CbpAeronave = cbpAeronave;
+	}
+	
+	
+}
---
+++ SAAC/src/Objetos/Compania.java
@@ -0,0 +1,31 @@
+
+public class Compania {
+	public String NomeCompania;
+	public int IdentificadorCompania;
+	
+	public Compania(String nomeCompania, int identificadorCompania) {
+		super();
+		NomeCompania = nomeCompania;
+		IdentificadorCompania = identificadorCompania;
+	}
+
+	public String getNomeCompania() {
+		return NomeCompania;
+	}
+
+	public void setNomeCompania(String nomeCompania) {
+		NomeCompania = nomeCompania;
+	}
+
+	public int getIdentificadorCompania() {
+		return IdentificadorCompania;
+	}
+
+	public void setIdentificadorCompania(int identificadorCompania) {
+		IdentificadorCompania = identificadorCompania;
+	}
+	
+	
+	
+	
+}
