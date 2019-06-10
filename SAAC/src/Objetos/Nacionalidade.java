0cc66804ce389e871469647dcfff046d6fbd1e25
Amanda Demétrio Alecrim <42013152+AmandaAlecrim@users.noreply.github.com>
2019-06-10 00:40:51 -0300

    Add files via upload

---
+++ SAAC/src/Objetos/Nacionalidade.java
@@ -0,0 +1,19 @@
+
+public class Nacionalidade {
+	
+	public String nacionalidade;
+	public int Pais;
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	public int getPais() {
+		return Pais;
+	}
+	public void setPais(int pais) {
+		Pais = pais;
+	}
+}
